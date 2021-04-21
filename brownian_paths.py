d2892d15b2aea33c19674b0e047f36d055ce5328
andreaippolito <48546752+andreaippolito@users.noreply.github.com>
2021-04-21 11:41:24 +0200

    Solved exercise2

--- brownian_paths.py
+++ brownian_paths.py
@@ -1,18 +1,23 @@
 """
-Module containing the class Brownian Paths, that is different realizion of Brownian Motions between 0 and a an ending poin
+Module containing the class Brownian Paths, that is different realization of Brownian Motions between 0 and
+an ending time
 """
 import numpy as np
+
+
 class BrownianPaths(object):
-    def __init__(self,no_of_paths, no_of_steps, starting_point, ending_point):
-        self.no_of_paths=no_of_paths
-        self.no_of_steps=no_of_steps
-        self.starting_point=starting_point
-        self.ending_point=ending_point
-        self.brownian_motion_paths=self.generate_paths_BM(self)
+    def __init__(self, no_of_paths, no_of_steps, starting_point, ending_point):
+        self.no_of_paths = no_of_paths
+        self.no_of_steps = no_of_steps
+        self.starting_point = starting_point
+        self.ending_point = ending_point
+        self.paths = self.generate_paths_bm()
     
-    def generate_paths_BM(self):
-        ###generate 
-        normal_values=np.random.normal(0.0, 1.0,(self.no_of_paths,self.no_of_steps))
-        return  temp
- 
-    
+    def generate_paths_bm(self):
+        # generate
+        dt = self.ending_point/float(self.no_of_steps+1)
+        normal_values = np.random.normal(0.0, 1.0, (self.no_of_paths, self.no_of_steps+1))
+        brownian_paths = np.zeros((self.no_of_paths, self.no_of_steps+1), float)
+        for i in range(0, self.no_of_steps, 1):
+            brownian_paths[:, i+1] = brownian_paths[:, i] + pow(dt, 0.5)*normal_values[:, i]
+        return brownian_paths
--- exercise2.py
+++ exercise2.py
@@ -4,63 +4,56 @@ Module in which the exercise 2 of the take home exam for the Computational Finan
 It is meant to be a self contained solution and therefore not relying on other code written by me.
 """
 import numpy as np
+import brownian_paths as bp
 
-def riemann_sum_brownian_motion(inferior, superior, brownian_motion_paths):
-    """It calculates the Riemann Sum of Brownion Motion using pseudo-randomly generated Brownian 
+
+def riemann_sum_brownian_motion(brownian_motion_paths):
+    """It calculates the Riemann Sum of Brownian Motion using pseudo-randomly generated Brownian
     Paths.
     inferior -- should be a real number representing the left boundary point of the integral.
     superior -- real number representing the right boundary point of the integral.
     brownian_motion_paths -- vectors of floats containing the points of pseudo Brownian Motion.
     """
-    brownian_motion_paths_size=np.matrix.size(brownian_motion_paths)
-    
-    dt=superior-inferior/float(number_of_steps)
-    riemann_sum=0
-    number_of_paths=int(np.size(brownian_motion_paths))
-    for i in range (0 , number_of_paths):
-        current_path=brownian_motion_paths[i, :]
-        riemann_sum= np.inner(current_path, dt*np.ones(number_of_steps))+riemann_sum
-    print(riemann_sum)
+
+    time_interval = (brownian_motion_paths.ending_point-brownian_motion_paths.starting_point) / \
+                    brownian_motion_paths.no_of_steps
+    riemann_sum = 0
+    for i in range(0, brownian_motion_paths.no_of_paths):
+        current_path = brownian_motion_paths.paths[i, :]
+        riemann_sum = np.inner(current_path, time_interval * np.ones(brownian_motion_paths.no_of_steps+1)) + riemann_sum
     return riemann_sum
 
 
-
-def ito_integral_calculator( function_left_point, stochastic_paths):
+def ito_integral_calculator(brownian_motion_paths):
     """
-    It returns the approximation of the Ito integral given the values of the integrated function evalutated 
+    It returns the approximation of the Ito integral given the values of the integrated function evaluated
     in the left point of each interval
     """
-    [number_of_paths, number_of_steps]=np.matrix.size(stochastic_paths)
-    brownian_motion_increments=np.zeros((number_of_paths, number_of_steps), float)
-    for i in range( 0 , number_of_steps-1):
-        brownian_motion_increments[:,i]=stochastic_paths[:, i+1]-stochastic_paths[:,i]
-    ito_sum=0
-    for i in range (0, number_of_paths):
-        ito_sum=np.inner(function_left_point, brownian_motion_increments[i, :])+ito_sum    
-    return ito_sum
-def GeneratePathsBM(NoOfPaths,NoOfSteps,T):
-    """
-    Method used to create paths of Brownian Motion until a given time.\n
-    T -- time at which the generation is stopped\n
-    NoOfPaths -- integer indicating the number of paths to generate\n
-    NoOfSteps -- integer indicating the number of intervals to consider between 0 and T 
-    """
-    # Fixing random seed
-    np.random.seed(1)
-    Z = np.random.normal(0.0,1.0,[NoOfPaths,NoOfSteps])
-    X = np.zeros([NoOfPaths, NoOfSteps+1])
-    time = np.zeros([NoOfSteps+1])
-    dt=T/float(NoOfSteps)
-    for i in range(0,NoOfSteps):
-        X[:,i+1] = X[:,i] +  np.power(dt, 0.5)*Z[:,i]
-        time[i+1] = time[i] +dt
-    return X
+    time_interval = (brownian_motion_paths.ending_point - brownian_motion_paths.starting_point) / \
+                    brownian_motion_paths.no_of_steps
+    brownian_motion_increments = np.zeros((brownian_motion_paths.no_of_paths, brownian_motion_paths.no_of_steps))
+    for i in range(0, brownian_motion_paths.no_of_steps):
+        brownian_motion_increments[:, i] = brownian_motion_paths.paths[:, i+1] - brownian_motion_paths.paths[:, i]
+
+    function_left_values = 5 - np.linspace(0, 5, brownian_motion_paths.no_of_steps, False)
+    ito_integral = 0
+    for i in range(0, brownian_motion_paths.no_of_paths):
+        current_path = brownian_motion_increments[i, :]
+        ito_integral = np.inner(function_left_values, current_path) + ito_integral
+    return ito_integral
+
+
 def main():
-    for i in range(4,12):
-        brownian_motion_paths=GeneratePathsBM(2^i,2^i, 5)
-        riemann_sum=riemann_sum_brownian_motion(0, 5, brownian_motion_paths)
-        ito_integral_calculator(0, brownian_motion_paths)
-    print(riemann_sum)
+    a = 1
+    b = 12
+    results = np.zeros((b-a, 2))
+    for i in range(a, b):
+        brownian_motion_paths = bp.BrownianPaths(pow(2, i), pow(2, i)-1, 0, 5)
+        riemann_sum = riemann_sum_brownian_motion(brownian_motion_paths)
+        results[i-a, 0] = riemann_sum
+        results[i-a, 1] = ito_integral_calculator(brownian_motion_paths)
+    print(results)
+
+
 if __name__ == '__main__':
     main()
-    
